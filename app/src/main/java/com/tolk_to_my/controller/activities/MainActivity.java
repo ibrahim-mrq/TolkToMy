776fd0072044302fb9f14e3cb15f11e576a7e6de
DESKTOP-NITBD9M\aboma <abo.mahroq@gmail.com>
2022-04-09 07:55:41 +0300

    init project

--- app/src/main/java/com/tolk_to_my/controller/activities/MainActivity.java
+++ app/src/main/java/com/tolk_to_my/controller/activities/MainActivity.java
@@ -3,6 +3,7 @@ package com.tolk_to_my.controller.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.tolk_to_my.R;
 
@@ -12,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
     }
 }
