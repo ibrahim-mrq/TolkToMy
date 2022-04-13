84006976b609eee43e6a106d5c448f88b70cd4b2
DESKTOP-NITBD9M\aboma <abo.mahroq@gmail.com>
2022-04-13 16:24:36 +0300

    update

--- app/src/main/java/com/tolk_to_my/controller/activities/ProfileActivity.java
+++ app/src/main/java/com/tolk_to_my/controller/activities/ProfileActivity.java
@@ -1,16 +1,31 @@
 package com.tolk_to_my.controller.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.tolk_to_my.R;
+import com.tolk_to_my.databinding.ActivityProfileBinding;
+import com.tolk_to_my.helpers.BaseActivity;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends BaseActivity {
+
+    ActivityProfileBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initView();
+    }
+
+    private void initView() {
+        binding.appbar.imgBack.setOnClickListener(view -> onBackPressed());
+        binding.appbar.tvTool.setText(getString(R.string.profile));
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
--- app/src/main/java/com/tolk_to_my/controller/activities/SettingActivity.java
+++ app/src/main/java/com/tolk_to_my/controller/activities/SettingActivity.java
@@ -1,16 +1,31 @@
 package com.tolk_to_my.controller.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.tolk_to_my.R;
+import com.tolk_to_my.databinding.ActivitySettingBinding;
+import com.tolk_to_my.helpers.BaseActivity;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends BaseActivity {
+
+    ActivitySettingBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        binding = ActivitySettingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        initView();
+    }
+
+    private void initView() {
+        binding.appbar.imgBack.setOnClickListener(view -> onBackPressed());
+        binding.appbar.tvTool.setText(getString(R.string.setting));
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
     }
 }
